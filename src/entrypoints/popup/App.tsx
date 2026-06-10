d8637a08f9910277ceaa81d8b7509c2f4be5167b
uc <u.chamikao2001@gmail.com>
2026-06-10 21:43:55 +0530

    Initial commit for v2 with wxt

---
+++ src/entrypoints/popup/App.tsx
@@ -0,0 +1,11 @@
+import InputView from '@/components/Inputview/InputView';
+
+function App() {
+  return (
+    <div className="w-full rounded-4xl">
+      <InputView />
+    </div>
+  );
+}
+
+export default App;
